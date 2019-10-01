3f35c43f45962206d55d35b07a8c70b6c0dc8549
Simon Ravary <sravary@hotmail.com>
2019-10-01 15:56:11 -0400

    Simon Says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
-#write your code here
+def echo(string)
+  string
+end
+
+def shout(string)
+  string.upcase
+end
+
+def repeat(string, num = 2)
+  ((string + " ") * num).strip
+end
+
+def start_of_word(string, num)
+  string[0...num]
+end
+
+def first_word(string)
+  string.split[0]
+end
+
+def titleize (string)
+  little_words = ['and', 'or', 'a', 'the', 'over']
+  final = string.split(' ').each_with_index do |word, index|
+    if little_words.include?(word) && index != 0
+      next
+    else
+      word[0] = word[0].capitalize
+    end
+  end
+  final.join(' ')
+end
+
