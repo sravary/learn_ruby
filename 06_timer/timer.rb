3513622ded72f254dbf5170c0e9d84576aaca182
Simon Ravary <sravary@hotmail.com>
2019-10-01 21:47:29 -0400

    time

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
-  #write your code here
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds = 0
+  end
+
+  def seconds=(time)
+    @seconds = time
+  end
+
+  def time_string
+    hour = (@seconds / 3600).to_s
+    minute = ((@seconds / 60) % 60).to_s
+    sec = (@seconds % 60).to_s
+    result = "#{hour.rjust(2, '0')}:#{minute.rjust(2, '0')}:#{sec.rjust(2, '0')}"
+  end
 end
